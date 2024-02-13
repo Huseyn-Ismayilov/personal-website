f0591f1c0252d912fad1499f8bf9e0b8dc6b32e7
Huseyn Ismayilov <131815459+Huseyn-Ismayilov@users.noreply.github.com>
2024-02-13 18:19:29 +0400

    padding px-4

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,6 +1,6 @@
 export default function Block(props) {
     return (
-        <div className="relative bg-white px-7 py-9 mb-7 border border-gray-50 md:rounded-xl overflow-hidden shadow-md -mx-4 md:mx-0">
+        <div className="relative bg-white px-4 md:px-7 py-9 mb-7 border border-gray-50 md:rounded-xl overflow-hidden shadow-md -mx-4 md:mx-0">
             {props.children}
         </div>
     )
